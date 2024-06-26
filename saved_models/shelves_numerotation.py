20b0e49b77c2a6aa5a5389189786a5c9915ff6e6
Mouaad BAKRIM <bakrim2mo@gmail.com>
2024-06-26 15:23:17 +0100

    upload

---
+++ saved_models/shelves_numerotation.py
@@ -0,0 +1,364 @@
+"""import json
+import tensorflow as tf
+from ultralytics import YOLO
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+from PIL import Image
+import numpy as np
+
+
+def summary(self, normalize=False, decimals=5):
+   
+    results = []
+    data = self.boxes.data.cpu().tolist()
+    h, w = self.orig_shape if normalize else (1, 1)
+    for i, row in enumerate(data):
+        box = {
+            "x1": round(row[0] / w, decimals),
+            "y1": round(row[1] / h, decimals),
+            "x2": round(row[2] / w, decimals),
+            "y2": round(row[3] / h, decimals),
+        }
+        conf = round(row[-2], decimals)
+        class_id = int(row[-1])
+        result = {"name": self.names[class_id], "class": class_id, "confidence": conf, "box": box}
+        results.append(result)
+    return results
+
+def process_results(results):
+    predictions = []
+    for result in results:
+        summary = result.summary()
+        predictions.extend(summary)
+    return predictions
+
+def sort_shelves(predictions):
+    sorted_shelves = sorted(predictions, key=lambda x: x['box']['y1'])
+    for i, shelf in enumerate(sorted_shelves, start=1):
+        shelf['name'] = f"Shelf_{i}"
+    return sorted_shelves
+
+def assign_items_to_shelves(sorted_shelves, detections):
+    for item in detections:
+        if item['name'].startswith("VTL150"):  # Vérifie si l'article est un VTL150
+            for shelf in sorted_shelves:
+                shelf_y1 = shelf['box']['y1']
+                shelf_y2 = shelf['box']['y2']
+                item_y1 = item['box']['y1']
+                item_y2 = item['box']['y2']
+                # Vérifie si les coordonnées `y1` et `y2` de l'article correspondent approximativement à celles de l'étagère
+                if shelf_y1 <= item_y1 <= shelf_y2 and shelf_y1 <= item_y2 <= shelf_y2:
+                    if "items" not in shelf:
+                        shelf["items"] = []  # Crée une liste d'articles pour cette étagère si elle n'existe pas encore
+                    shelf["items"].append(item)  # Ajoute l'article à la liste d'articles de l'étagère
+                    break  # Sort de la boucle dès que l'article est attribué à une étagère
+    return sorted_shelves
+
+
+def Shelves_detection(model_path, image_path):
+    model = YOLO(model_path)
+    results = model(image_path)
+    detections = process_results(results)
+    sorted_shelves = sort_shelves([d for d in detections if d['name'].startswith("Shelf")])
+
+    # Associer les articles détectés aux étagères correspondantes
+    sorted_shelves_with_items = assign_items_to_shelves(sorted_shelves, detections)
+
+    # Enregistre les étagères triées avec les articles associés dans un fichier JSON
+    with open('detect_shelves_sorted_with_items.json', 'w') as json_file:
+        json.dump(sorted_shelves_with_items, json_file, indent=4)
+
+def Fridge_Classification(model_path, image_path, class_names):
+
+    img_height = 600
+    img_width = 449
+
+    model = load_model(model_path)
+    image_file = image_path
+
+    img = image.load_img(image_file, target_size=(img_height,img_width))
+    img.show()
+
+    img_array = image.img_to_array(img)
+    #print(type(img_array))
+    img_array = np.expand_dims(img_array, axis=0)
+    #print(img_array)
+
+    prediction = model.predict(img_array)
+    predicted_class = np.argmax(prediction)
+    class_names = class_names[predicted_class]
+    score = 100*np.max(prediction)
+    result = {
+        "predicted_class": class_names,
+        "confidence_score": score
+    }
+
+
+# Exemple d'utilisation de la fonction
+SHELVES_PREDICTION_MODEL = 'shelves_detection.pt'
+IMAGE_PATH = "C:/Users/VICTUS/Pictures/Dataset_Fridge/mean_images/84c81b29-dd12-460c-ad24-ef1596252c87.jpg"
+Shelves_detection(SHELVES_PREDICTION_MODEL, IMAGE_PATH)
+
+# Exemple d'utilisation de la fonction avec classification models
+image_path = "C:/Users/VICTUS/Pictures/Dataset_Fridge/mean_images/84c81b29-dd12-460c-ad24-ef1596252c87.jpg"
+
+USABLE_MODEL_PATH = './usage_model.keras'
+Usableclass_names = ['unusable', 'usable']
+Fridge_Classification(USABLE_MODEL_PATH, image_path, Usableclass_names)
+
+DoorStatus_MODEL_PATH = "ds_model.keras"
+DoorStatusclass_names = ['closed', 'opened']
+Fridge_Classification(DoorStatus_MODEL_PATH, image_path, DoorStatusclass_names)
+
+"""
+
+import json
+import numpy as np
+from ultralytics import YOLO
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+from PIL import Image
+
+
+def summary(self, normalize=False, decimals=5):
+    """Convert the results to a summarized format."""
+    results = []
+    data = self.boxes.data.cpu().tolist()
+    h, w = self.orig_shape if normalize else (1, 1)
+    for i, row in enumerate(data):
+        box = {
+            "x1": round(row[0] / w, decimals),
+            "y1": round(row[1] / h, decimals),
+            "x2": round(row[2] / w, decimals),
+            "y2": round(row[3] / h, decimals),
+        }
+        conf = round(row[-2], decimals)
+        class_id = int(row[-1])
+        result = {"name": self.names[class_id], "class": class_id, "confidence": conf, "box": box}
+        results.append(result)
+    return results
+
+def process_results(results):
+    predictions = []
+    for result in results:
+        summary = result.summary()
+        predictions.extend(summary)
+    return predictions
+
+def sort_shelves(predictions):
+    sorted_shelves = sorted(predictions, key=lambda x: x['box']['y1'])
+    for i, shelf in enumerate(sorted_shelves, start=1):
+        shelf['name'] = f"Shelf_{i}"
+    return sorted_shelves
+
+
+def assign_items_to_shelves(sorted_shelves, detections):
+    for item in detections:
+        if not item['name'].startswith("Shelf") and not item['name'].startswith("Fridge"):  # Vérifie si l'élément n'est pas une étagère ou un réfrigérateur
+            assigned = False
+            for shelf in sorted_shelves:
+                shelf_y1 = shelf['box']['y1']
+                shelf_y2 = shelf['box']['y2']
+                item_y1 = item['box']['y1']
+                item_y2 = item['box']['y2']
+                # Vérifie si les coordonnées `y1` et `y2` de l'article correspondent approximativement à celles de l'étagère
+                if shelf_y1 <= item_y1 <= shelf_y2 and shelf_y1 <= item_y2 <= shelf_y2:
+                    if "items" not in shelf:
+                        shelf["items"] = []  # Crée une liste d'articles pour cette étagère si elle n'existe pas encore
+                    shelf["items"].append(item)  # Ajoute l'article à la liste d'articles de l'étagère
+                    assigned = True
+                    break  # Sort de la boucle dès que l'article est attribué à une étagère
+            if not assigned:
+                # Si l'article n'est pas attribué à une étagère, l'ajoute directement aux résultats triés
+                sorted_shelves.append(item)
+    return sorted_shelves
+
+ALIGNMENT_MODEL_PATH = "./articles_alignment.keras"
+Alignment_class_names = ['not-ranged', 'ranged']
+
+PROFILPLASTIC_MODEL_PATH = "./profil_platic.keras"
+profilplastic_class_names = ['available', 'unavailable']
+
+FONDARTICLES_MODEL_PATH = "./fond_articles.keras"
+Fondarticlesclass_names = ['absent', 'present']
+
+
+def Item_detection(model_path, image_path):
+    model = YOLO(model_path)
+    results = model(image_path)
+    detections = process_results(results)
+    return detections
+    """for result in results:
+        print(results)
+        result.show() 
+        """
+def Shelves_detection(model_path, image_path):
+    model = YOLO(model_path)
+    results = model(image_path)
+    detections = process_results(results)
+    sorted_shelves = sort_shelves([d for d in detections if d['name'].startswith("Shelf")])
+     
+    # Classer l'alignement des articles pour chaque étagère détectée
+    for shelf in sorted_shelves:
+        shelf_image = crop_shelf_image(image_path, shelf['box'])  # Fonction à implémenter pour recadrer l'image de l'étagère
+        
+        shelf_alignment_result = classify_shelf_alignment(ALIGNMENT_MODEL_PATH, shelf_image, Alignment_class_names)
+        shelf['alignment_result'] = shelf_alignment_result
+
+        shelf_profil_result = classify_shelf_profil(PROFILPLASTIC_MODEL_PATH, shelf_image, profilplastic_class_names)
+        shelf['shelf_profil_result'] = shelf_profil_result
+
+        shelf_fond_result = classify_shelf_fond(FONDARTICLES_MODEL_PATH, shelf_image, Fondarticlesclass_names)
+        shelf['shelf_fond_result'] = shelf_fond_result
+    # Associer les articles détectés aux étagères correspondantes
+    sorted_shelves_with_items = assign_items_to_shelves(sorted_shelves, detections)
+
+    return sorted_shelves_with_items
+
+
+def Fridge_Classification(model_path, image_path, class_names):
+    img_height = 600
+    img_width = 449
+
+    model = load_model(model_path)
+    image_file = image_path
+
+    img = image.load_img(image_file, target_size=(img_height,img_width))
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+
+    prediction = model.predict(img_array)
+    predicted_class = np.argmax(prediction)
+    class_name = class_names[predicted_class]
+    score = 100*np.max(prediction)
+
+    result = {
+        "predicted_class": class_name,
+        "confidence_score": score
+    }
+
+    return result
+
+def classify_shelf_alignment(model_path, shelf_image, class_name):
+    img_height = 100
+    img_width = 200
+
+    model = load_model(model_path)
+
+    img_array = image.img_to_array(shelf_image)
+    img_array = np.expand_dims(img_array, axis=0)
+
+    prediction = model.predict(img_array)
+    predicted_class = np.argmax(prediction)
+    class_name = Alignment_class_names[predicted_class]
+    score = 100 * np.max(prediction)
+
+    result = {
+        "predicted_class": class_name,
+        "confidence_score": score
+    }
+
+    return result
+
+def classify_shelf_profil(model_path, shelf_image, class_name):
+    img_height = 100
+    img_width = 200
+
+    model = load_model(model_path)
+
+    img_array = image.img_to_array(shelf_image)
+    img_array = np.expand_dims(img_array, axis=0)
+
+    prediction = model.predict(img_array)
+    predicted_class = np.argmax(prediction)
+    class_name = profilplastic_class_names[predicted_class]
+    score = 100 * np.max(prediction)
+
+    result = {
+        "predicted_class": class_name,
+        "confidence_score": score
+    }
+
+    return result
+
+def classify_shelf_fond(model_path, shelf_image, class_name):
+    img_height = 100
+    img_width = 200
+
+    model = load_model(model_path)
+
+    img_array = image.img_to_array(shelf_image)
+    img_array = np.expand_dims(img_array, axis=0)
+
+    prediction = model.predict(img_array)
+    predicted_class = np.argmax(prediction)
+    class_name = Fondarticlesclass_names[predicted_class]
+    score = 100 * np.max(prediction)
+
+    result = {
+        "predicted_class": class_name,
+        "confidence_score": score
+    }
+
+    return result
+def crop_shelf_image(image_path, box_coordinates):
+
+    # Chargement de l'image
+    image = Image.open(image_path)
+
+    # Récupération des coordonnées de la boîte englobante
+    x1, y1, x2, y2 = box_coordinates['x1'], box_coordinates['y1'], box_coordinates['x2'], box_coordinates['y2']
+    x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+
+    # Recadrage de l'image selon les coordonnées de la boîte englobante
+    cropped_image = image.crop((x1, y1, x2, y2))
+
+    # Enregistrement de l'image recadrée
+    return cropped_image
+
+
+
+
+# Exemple d'utilisation des fonctions
+SHELVES_PREDICTION_MODEL = 'shelves_detection.pt'
+ITEM_PREDICTION_MODEL = 'Item_detection.pt'
+
+FRIDGE_CLASSIFICATION_MODEL_1 = './usage_model.keras'
+FRIDGE_CLASSIFICATION_MODEL_2 = './brand_model.keras'
+FRIDGE_CLASSIFICATION_MODEL_3 = "./model_c.keras"
+FRIDGE_CLASSIFICATION_MODEL_4 = './DoorStatus_model.keras'
+
+#IMAGE_PATH = "C:/Users/VICTUS/Pictures/Dataset_Fridge/Object_detection/Dataset_v1/61e203ee-79cf-4316-a520-a937f684944c.jpg"
+IMAGE_PATH = "C:/Users/VICTUS/Pictures/Dataset_Fridge/json_file/testImg.jpg"
+Usableclass_names = ['unusable', 'usable']
+Brandclass_names = ['orangina', 'sidi ali', 'unknown']
+Stateclass_names = ['bad', 'good','unknown']
+DoorStatusclass_names = ['closed', 'opened']
+
+
+
+
+# Classification de l'image
+usable_result = Fridge_Classification(FRIDGE_CLASSIFICATION_MODEL_1, IMAGE_PATH, Usableclass_names)
+Brand_result = Fridge_Classification(FRIDGE_CLASSIFICATION_MODEL_2, IMAGE_PATH, Brandclass_names)
+condition_result = Fridge_Classification(FRIDGE_CLASSIFICATION_MODEL_3, IMAGE_PATH, Stateclass_names)
+DoorSatatus_result = Fridge_Classification(FRIDGE_CLASSIFICATION_MODEL_4, IMAGE_PATH, DoorStatusclass_names)
+
+# Détection des étagères
+shelf_results = Shelves_detection(SHELVES_PREDICTION_MODEL, IMAGE_PATH)
+item_results = Item_detection(ITEM_PREDICTION_MODEL, IMAGE_PATH)
+item_results_serializable = [item_result.to_dict() for item_result in item_results]
+
+# Classification des etageres 
+
+
+#shelf_alignment_result = classify_shelf_alignment(ALIGNMENT_MODEL_PATH, crop_shelf_image(), Alignment_class_names)
+
+# Combinaison des résultats
+combined_result = {"usable_result": usable_result,"Brand_result":Brand_result, 
+                   "condition_result":condition_result,"DoorSatatus_result":DoorSatatus_result,
+                   "shelf_results": shelf_results, "item_results":item_results_serializable}
+
+# Enregistrement dans un fichier JSON
+with open('testfridge_results.json', 'w') as json_file:
+    json.dump(combined_result, json_file, indent=4)
